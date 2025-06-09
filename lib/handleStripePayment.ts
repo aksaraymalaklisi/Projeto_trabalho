cb981c8a85898c693097a380644089676fe2856f
unknown <pcoutinhoduarte@gmail.com>
2025-06-09 13:06:52 -0300

    teste trabalho projeto

--- lib/handleStripePayment.ts
+++ lib/handleStripePayment.ts
@@ -1,10 +1,9 @@
-
 import { handleStripePayment } from "@/lib/handleStripePayment";
 import { loadStripe } from '@stripe/stripe-js';
 
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
 
-export const handleStripePayment = async (pkgPrice: number) => {
+export const handleStripe = async (pkgPrice: number) => {
   const stripe = await stripePromise;
 
   const response = await fetch('/api/create-checkout-session', {
